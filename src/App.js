b3095872ac2ea4e3f614c7dc73cbef527328ef1d
alexandermcmillan <mcmillancontact@protonmail.com>
2024-05-31 13:53:43 -0700

    initial components created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return <div className="App">REACTTTTTTT</div>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+
+  function Header() {
+    return <div>This is the header</div>;
+  }
+
+  function Menu() {
+    return <div>This is the menu</div>;
+  }
+
+  function Footer() {
+    return <div>This is the footer</div>;
+  }
 }
 
 export default App;
