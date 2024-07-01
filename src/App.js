180c34168c8199da37c95b49866cc66dee2573cc
alexandermcmillan <mcmillancontact@protonmail.com>
2024-06-30 18:53:10 -0700

    newitemfrom component split

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Header from "./Header";
 import Menu from "./Menu";
+import NewItemForm from "./NewItemForm";
 
 function App() {
   const [bill, setBill] = useState(0);
@@ -55,83 +56,6 @@ function App() {
             {item.name} - ${item.price}
           </h2>
         </p>
-      </div>
-    );
-  }
-
-  function NewItemForm() {
-    const [dishName, setDishName] = useState("");
-    const [price, setPrice] = useState(13);
-    const [description, setDescription] = useState("A Delicious Creation");
-
-    function handleSubmit(e) {
-      e.preventDefault();
-      const newItem = {
-        id: uuidv4(),
-        name: dishName,
-        description: description,
-        price: price,
-        img: "https://roadfood.com/wp-content/uploads/2022/03/Hodad-double-cheeseburger.jpg",
-      };
-      fetch("http://127.0.0.1:3000/menuData", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newItem),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            return response.text().then((text) => {
-              throw new Error(text);
-            });
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setMenu((prevMenu) => [...prevMenu, data]);
-        })
-        .catch((error) => {
-          console.error("Error:", error.message);
-        });
-    }
-
-    return (
-      <div className="newitemForm">
-        <h2>Make Your Own Dish!</h2>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={dishName}
-              onChange={(e) => setDishName(e.target.value)}
-            />
-          </label>
-
-          <label>
-            Description:
-            <input
-              type="text"
-              name="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </label>
-          <label>
-            Price:
-            <input
-              type="text"
-              name="price"
-              value={price}
-              onChange={(e) => setPrice(parseFloat(e.target.value))}
-            />
-          </label>
-          <p>
-            <button>Submit</button>
-          </p>
-        </form>
       </div>
     );
   }
